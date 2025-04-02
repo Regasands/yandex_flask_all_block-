0b5f2e71ac7d380fd0c2ec78cbbe77d352102e46
reqwe <cvindergocvin@gmail.com>
2025-04-02 21:43:50 +0300

    add task 7 for sqlalchemy

--- flask_sqlalchemy/7/app.py
+++ flask_sqlalchemy/7/app.py
@@ -2,5 +2,23 @@ import sqlalchemy
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 from data.db_session import *
+from data.jobs import Jobs
 
-global_init('db/new.db')
+from flask import Flask, render_template
+
+
+global_init('db/blogs.db')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start_home():
+    session = create_session()
+    data = session.query(Jobs).all()
+    print(data)
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
