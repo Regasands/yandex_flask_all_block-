d1aa324370ec9179eb94c51338c92576508907ce
regqwe <rego613@yandex.ru>
2025-04-19 10:55:26 +0300

    add_new_task

--- telegram/8.py
+++ telegram/8.py
@@ -2,6 +2,7 @@ import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 import json
 import os
+import random
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -47,31 +48,41 @@ async def get_file(update, context):
     return ConversationHandler.END
 
 async def start(update, context):
-    context.user_data['number'] = 0
     context.user_data['properly'] = 0
+    answer = [1, 2, 3, 4, 5, 6, 7 ,8, 9, 0]
+    next_number = random.choice(answer)
+
+    context.user_data['next'] = next_number
     question = context.user_data['json_data']
     
     await update.message.reply_text(f"Отлино ответь на первый вопрос")
-    await update.message.reply_text(f'{question[0]['question']}')
+    await update.message.reply_text(f'{question[next_number]['question']}')
+    context.user_data['list_answer'] = [next_number]
     return 1
 
 async def asnwer(update, context):
     try:
-        number = context.user_data['number']
-        question = context.user_data['json_data'][number]
+        numbers = context.user_data['list_answer']
+        answer = [1, 2, 3, 4, 5, 6, 7 ,8, 9, 0]
+        number = list(filter(lambda x: x not in numbers, answer))
+        if number == []:
+            await update.message.reply_text(f'Отлично, тест завершен коли-во правильных ответов {context.user_data['properly']}, пройдитест заново /start')
+            return ConversationHandler.END
+
+        next_number = random.choice(number)
+        last_number = context.user_data['next']
+        question = context.user_data['json_data'][last_number]
         answer = update.message.text
         if answer == question['response']:
             await update.message.reply_text('Отлично, верно ')
             context.user_data['properly'] += 1
         else:
             await update.message.reply_text(f'Не верно, ответ {question["response"]} ')
-        if number + 1 >= len(context.user_data['json_data']):
-            await update.message.reply_text(f'Отлично, тест завершен коли-во правильных ответов {context.user_data['properly'] }')
-            return ConversationHandler.END
-        else:
-            context.user_data['number'] += 1
 
-            await update.message.reply_text(f'Следующий вопрос, {context.user_data['json_data'][number + 1]['question']}')
+        context.user_data['next'] = next_number
+        numbers.append(next_number)
+        context.user_data['list_answer'] = numbers
+        await update.message.reply_text(f'Следующий вопрос, {context.user_data['json_data'][next_number]['question']}')
 
         return 1
     except Exception as e:
